f98b56a96d9d8eb8c714640dfeecfea1d9cdc41b
LuizHillebrande <luizhill@edu.unifil.br>
2024-12-12 16:52:19 -0300

    diario_sp

--- diario_sp.py
+++ diario_sp.py
@@ -7,11 +7,16 @@ from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 import tkinter as tk
 from tkinter import ttk, messagebox
+import re
 
 def iniciar_raspagem_compras_gov(dia_inicio, mes_inicio, ano_inicio, dia_fim, mes_fim, ano_fim):
     driver = webdriver.Chrome()
     driver.get('https://www.imprensaoficial.com.br/ENegocios/BuscaENegocios_14_1.aspx#12/12/2024')
     sleep(2)
+
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.append(["Link", "CNPJ", "Razão Social"])
 
     # Preencher Status ENCERRADA
     encerrada = WebDriverWait(driver, 5).until(
@@ -38,7 +43,75 @@ def iniciar_raspagem_compras_gov(dia_inicio, mes_inicio, ano_inicio, dia_fim, me
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, xpath_ano_fim))).click()
 
     sleep(3)
-    messagebox.showinfo("Sucesso", "As datas foram preenchidas com sucesso!")
+    
+    buscar = WebDriverWait(driver,5).until(
+        EC.element_to_be_clickable((By.XPATH,"//input[@onclick='return verify();']"))
+    )
+    buscar.click()
+
+    links = driver.find_elements(By.XPATH, "//a[contains(@id, 'ResultadoBusca_dtgResultadoBusca_hlkObjeto')]")
+
+    # Coletar e remover duplicatas usando 'dict.fromkeys'
+    links_unicos = list(dict.fromkeys([link.get_attribute('href') for link in links]))
+
+    # Imprimir os resultados
+    print(f"Total de links encontrados: {len(links_unicos)}")
+    for i, link in enumerate(links_unicos, start=1):
+        print(f"Acessando Link {i}: {link}")
+        driver.get(link)
+
+        homologacao_links = driver.find_elements(By.XPATH, "//a[contains(@onclick, 'HOMOLOGAÇÃO')]")
+
+        if homologacao_links:
+            for homologacao_link in homologacao_links:
+                homologacao_link.click()
+                sleep(2)
+
+                main_window = driver.current_window_handle
+
+                
+                WebDriverWait(driver, 10).until(EC.new_window_is_opened(driver.window_handles))
+
+                #troca para janela do pop up
+                all_windows = driver.window_handles
+                popup_window = [window for window in all_windows if window != main_window][0]
+                driver.switch_to.window(popup_window)
+
+                try:
+                    
+                    detalhes_texto = driver.find_element(By.ID, "content_content_content_DetalheEvento_lblSintesePublicacao").text
+
+                    cnpj_match = re.search(r'CNPJ\s*-\s*(\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})', detalhes_texto)
+                    razao_social_match = re.search(r'EMPRESA\s*-\s*(.*?)(?=\s*CNPJ)', detalhes_texto)
+
+                    cnpj = cnpj_match.group(1) if cnpj_match else "Não encontrado"
+                    razao_social = razao_social_match.group(1) if razao_social_match else "Não encontrado"
+
+                    # Salva os dados na planilha
+                    ws.append([link, cnpj, razao_social])
+                    print(f"Dados capturados: CNPJ={cnpj}, Razão Social={razao_social}")
+
+                except Exception as e:
+                    print(f"Erro ao processar o link {link}: {e}")
+                
+                # Fechar o pop-up
+                driver.close()
+
+                # Voltar para a janela principal
+                driver.switch_to.window(main_window)
+
+                sleep(2)  # Tempo para garantir que a nova página carregue corretamente
+
+        else:
+            print(f"Não encontrado link de HOMOLOGAÇÃO na página {link}")
+                
+   
+    wb.save("dados_vencedores_diario_sp.xlsx")
+    print("Planilha salva como 'dados_vencedores_diario_sp.xlsx'")
+    
+    sleep(10)
+    driver.quit()
+    print("Navegador fechado.")
 
 def capturar_datas():
     dia_inicio = combo_dia_inicio.get()
