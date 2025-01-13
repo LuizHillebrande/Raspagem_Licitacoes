11ccd3e994bef1033f400337650e49efd2bca9bc
LuizHillebrande <luizhill@edu.unifil.br>
2025-01-12 22:27:50 -0300

    raspando alternando, erro qnd n tem element details

--- compras.py
+++ compras.py
@@ -13,68 +13,45 @@ from selenium.webdriver.common.action_chains import ActionChains
 def verifica_ajudicacao(driver):
                 situacao_texto = ""
                 adjudicada_encontrada = False
-                elementos_situacao = WebDriverWait(driver, 10).until(
-                    EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div[data-test='situacao-proposta']"))
-                )
+                try:
+                    elementos_situacao = WebDriverWait(driver, 5).until(
+                        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div[data-test='situacao-proposta']"))
+                    )
 
-                for elemento_situacao in elementos_situacao:
-                    # Obtém o texto do elemento
-                    situacao_texto = elemento_situacao.text.strip()
+                    
 
-                    # Verifica se o texto é "Adjudicada"
-                    if situacao_texto.lower() == "adjudicada":
-                        adjudicada_encontrada = True
-                        print("A proposta está adjudicada. Realizando ações subsequentes...")
+                    for elemento_situacao in elementos_situacao:
+                        # Obtém o texto do elemento
+                        situacao_texto = elemento_situacao.text.strip()
 
-                        # Agora, encontra o contêiner de proposta
-                        try:
-                            container = elemento_situacao.find_element(By.XPATH, "./ancestor::div[contains(@data-test, 'propostaItemEmSelecaoFornecedores')]")
-                            # Espera até que o botão de expansão esteja visível e clicável
-                            container_cnpj = WebDriverWait(container, 10).until(
-                                EC.visibility_of_element_located((By.XPATH, ".//span[@data-test='identificacao-participante']"))
-                            )
-                            cnpj = container_cnpj.text
-                            print('cnpj encontrado:',cnpj)
-                        except Exception as e:
-                            print(f"Erro ao encontrar o botão de expansão: {e}")
-                            continue 
+                        # Verifica se o texto é "Adjudicada"
+                        if situacao_texto.lower() == "adjudicada":
+                            adjudicada_encontrada = True
+                            print("A proposta está adjudicada. Realizando ações subsequentes...")
+
+                            # Agora, encontra o contêiner de proposta
+                            try:
+                                container = elemento_situacao.find_element(By.XPATH, "./ancestor::div[contains(@data-test, 'propostaItemEmSelecaoFornecedores')]")
+                                # Espera até que o botão de expansão esteja visível e clicável
+                                container_cnpj = WebDriverWait(container, 10).until(
+                                    EC.visibility_of_element_located((By.XPATH, ".//span[@data-test='identificacao-participante']"))
+                                )
+                                cnpj = container_cnpj.text
+                                print('cnpj encontrado:',cnpj)
+                            except Exception as e:
+                                print(f"Erro ao encontrar o botão de expansão: {e}")
+                                continue 
+                except Exception as e:
+                    print(f"Erro ao buscar elementos de situação: {e}")
+                    return situacao_texto, False  # Caso não encontrar a situação
+
+                       
+
                 return situacao_texto, adjudicada_encontrada
 
 
-def iniciar_raspagem_compras_gov(ano):
-    # Inicializa o navegador com undetected_chromedriver
-    driver = uc.Chrome()
-    driver.get('https://cnetmobile.estaleiro.serpro.gov.br/comprasnet-web/public/compras')
-    driver.maximize_window()
-    sleep(random.uniform(1, 3))  # Delay aleatório
-
-    # Criar a planilha para salvar os dados
-    wb = openpyxl.Workbook()
-    ws = wb.active
-    ws.append(["Link", "CNPJ", "Razão Social"])  
-
-    # Interage com o filtro
-    filtro = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[@class='p-radiobutton-box']"))
-    )
-    filtro.click()
-    sleep(random.uniform(1, 3))  # Delay aleatório
-
-
-    input_ano = WebDriverWait(driver,5).until(
-        EC.element_to_be_clickable((By.XPATH,"//input[@placeholder='Ex: 102021']"))
-    )
-    input_ano.send_keys(ano)
-    sleep(random.uniform(1, 3))
-
-        # Clica no botão pesquisar
-    button_pesquisar = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@class='br-button is-primary']"))
-    )
-    button_pesquisar.click()
-    sleep(random.uniform(1, 3))  # Delay aleatório
-
-    # Coleta os links únicos
+def raspar_pagina(driver, wb):
+     # Coleta os links únicos
     elements = driver.find_elements(By.XPATH, "//i[@class='fa fa-tasks']")
     qtde_apps_card = len(driver.find_elements(By.XPATH, "//i[@class='fa fa-tasks']"))
 
@@ -118,15 +95,13 @@ def iniciar_raspagem_compras_gov(ano):
                 sleep(3)
                 
                 situacao_texto, adjudicada_encontrada = verifica_ajudicacao(driver)
-
-                if cont_element_details > 0 and not adjudicada_encontrada:
-                        print('Tinha mais que 1 botao, porem o 1 nao tinha adjudicada')
+                
+                if not adjudicada_encontrada:
+                        print('Nenhuma proposta adjudicada')
                         driver.back()
                         sleep(2)
                         driver.back()
                         sleep(3)
-                        break
-                    
                 else:
                     print('tem apenas 1 item, voltando pra pagina inicial')
                     driver.back()
@@ -143,16 +118,61 @@ def iniciar_raspagem_compras_gov(ano):
                 if index == qtde_apps_card - 1:
                     print(f'Elemento atual: {index}, último elemento: {(qtde_apps_card) - 1}')
                     next_page.click()
+                    sleep(3)
+                    raspar_pagina(driver, wb)
                 else:
                     break
                     
                         
         else:
             print("Nenhum elemento encontrado.")
+            driver.back()
+            sleep(2)
+            
     wb.save("dados_vencedores_portal_compras_publicas.xlsx")
     sleep(15)
     driver.quit()
     messagebox.showinfo("Concluído", "Raspagem concluída com sucesso!")
+
+def iniciar_raspagem_compras_gov(ano):
+    # Inicializa o navegador com undetected_chromedriver
+    driver = uc.Chrome()
+    driver.get('https://cnetmobile.estaleiro.serpro.gov.br/comprasnet-web/public/compras')
+    driver.maximize_window()
+    sleep(random.uniform(1, 3))  # Delay aleatório
+
+    # Criar a planilha para salvar os dados
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.append(["Link", "CNPJ", "Razão Social"])  
+
+    # Interage com o filtro
+    filtro = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@class='p-radiobutton-box']"))
+    )
+    filtro.click()
+    sleep(random.uniform(1, 3))  # Delay aleatório
+
+
+    input_ano = WebDriverWait(driver,5).until(
+        EC.element_to_be_clickable((By.XPATH,"//input[@placeholder='Ex: 102021']"))
+    )
+    input_ano.send_keys(ano)
+    sleep(random.uniform(1, 3))
+
+        # Clica no botão pesquisar
+    button_pesquisar = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@class='br-button is-primary']"))
+    )
+    button_pesquisar.click()
+    sleep(random.uniform(1, 3))  # Delay aleatório
+
+    raspar_pagina(driver, wb)
+
+    wb.save("dados_vencedores_portal_compras_publicas.xlsx")
+    sleep(5)
+    driver.quit()
+    messagebox.showinfo("Concluído", "Raspagem concluída com sucesso!")    
 
 def iniciar():
     ano = entry_ano.get()  # Obtém o valor digitado na interface
