c729c0e1ce64990684c554d22a8fe1a036fa3028
LuizHillebrande <luizhill@edu.unifil.br>
2025-01-22 20:06:14 -0300

    eliminar cnpj corretamente

--- raspagem_emails.py
+++ raspagem_emails.py
@@ -11,7 +11,6 @@ import customtkinter as ctk
 import glob  # Para buscar arquivos com padrão
 from tkinter import messagebox
 
-
 def criar_interface_raspagem_emails():
     janela = ctk.CTk()  # Janela principal
     janela.geometry("500x300")
@@ -55,14 +54,10 @@ def salvar_emails(resultados):
     """
     Salva os resultados no arquivo de e-mails, criando novos arquivos se o número de linhas exceder 2000.
     """
-    # Definir o nome base do arquivo
     nome_arquivo_base = "emails_vencedores"
+    arquivos_existentes = glob.glob(f"{nome_arquivo_base}_*.xlsx")
+    indice_arquivo = len(arquivos_existentes) + 1
 
-    # Verificar o número total de registros no arquivo existente
-    arquivos_existentes = glob.glob(f"{nome_arquivo_base}_*.xlsx")  # Busca todos os arquivos existentes com o padrão de nome
-    indice_arquivo = len(arquivos_existentes) + 1  # Define o índice do próximo arquivo a ser salvo
-
-    # Se não houver arquivos, salva no arquivo padrão
     if not arquivos_existentes:
         nome_arquivo = f"{nome_arquivo_base}.xlsx"
     else:
@@ -70,78 +65,21 @@ def salvar_emails(resultados):
 
     df_resultados = pd.DataFrame(resultados)
 
-    # Se o arquivo já existir, verifica quantas linhas já existem e onde continuar
     if os.path.exists(nome_arquivo):
         df_existente = pd.read_excel(nome_arquivo)
         if len(df_existente) >= 2000:
             indice_arquivo += 1
             nome_arquivo = f"{nome_arquivo_base}_{indice_arquivo}.xlsx"
-    
-    # Salva os e-mails no arquivo
+
     df_resultados.to_excel(nome_arquivo, index=False)
     print(f"E-mails salvos no arquivo '{nome_arquivo}'.")
 
-
 def consulta_cnpj_gratis(label_contador, janela):
-    # Usando glob para buscar arquivos que começam com 'dados_' e terminam com '.xlsx'
     arquivos = glob.glob("cnpjs_*.xlsx")
     
     if not arquivos:
         messagebox.showwarning("Atenção", "O arquivo 'cnpjs_unicos.xlsx' não foi encontrado! Elimine os CNPJ duplicados!")
-        return  # Para a execução da função
-
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    def limpar_cnpj(cnpj):
-        return ''.join(filter(str.isdigit, str(cnpj)))  # Mantém apenas dígitos
-
-    total_emails = 0
-    resultados = []
-    emails_unicos = set()
-
-    try:
-        for arquivo in arquivos:
-            if os.path.exists(arquivo):
-                df = pd.read_excel(arquivo)
-
-                for cnpj in df.iloc[:, 0]:  # Segunda coluna
-                    cnpj_limpo = limpar_cnpj(cnpj)
-                    if cnpj_limpo:
-                        link = f"https://consulta.guru/consultar-cnpj-gratis/{cnpj_limpo}"
-                        driver.get(link)
-                        sleep(5)  # Aumentar o tempo de espera para garantir que a página carregue completamente
-
-                        try:
-                            email_element = driver.find_element(By.XPATH, '//p[contains(text(), "@")]')
-                            email = email_element.text.strip()
-
-                            if email not in emails_unicos:
-                                emails_unicos.add(email)
-                                resultados.append({"CNPJ": cnpj_limpo, "Email": email})
-                                total_emails += 1
-                                label_contador.configure(text=f"E-mails raspados: {total_emails}")
-                                janela.update()
-                        except Exception:
-                            print(f"Erro ao encontrar o e-mail para o CNPJ: {cnpj_limpo}")
-                            pass  # Se não achar o email, passa para o próximo CNPJ
-
-                        salvar_emails(resultados)
-                        sleep(12)
-                        
-
-        salvar_emails(resultados)
-    finally:
-        driver.quit()
-
-def consulta_cnpj_ja(label_contador, janela):
-    # Usando glob para buscar arquivos que começam com 'dados_' e terminam com '.xlsx'
-    arquivos = glob.glob("cnpjs_*.xlsx")
-
-    if not arquivos:
-        messagebox.showwarning("Atenção", "O arquivo 'cnpjs_unicos.xlsx' não foi encontrado! Elimine os CNPJ duplicados!")
-        return  # Para a execução da função
+        return
 
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
@@ -159,12 +97,67 @@ def consulta_cnpj_ja(label_contador, janela):
             if os.path.exists(arquivo):
                 df = pd.read_excel(arquivo)
 
-                for cnpj in df.iloc[:, 0]:
+                for cnpj in df.iloc[:, 0]:  # Segunda coluna
+                    cnpj_limpo = limpar_cnpj(cnpj)
+                    if cnpj_limpo:
+                        link = f"https://consulta.guru/consultar-cnpj-gratis/{cnpj_limpo}"
+                        driver.get(link)
+                        sleep(5)
+
+                        try:
+                            email_element = driver.find_element(By.XPATH, '//p[contains(text(), "@")]')
+                            email = email_element.text.strip()
+
+                            # Capturando a razão social
+                            razao_social_element = driver.find_element(By.XPATH, '//div[@id="overview"]//h1')
+                            razao_social = razao_social_element.text.strip()
+
+                            if email not in emails_unicos:
+                                emails_unicos.add(email)
+                                resultados.append({"CNPJ": cnpj_limpo, "Email": email, "Razao Social": razao_social})
+                                total_emails += 1
+                                label_contador.configure(text=f"E-mails raspados: {total_emails}")
+                                janela.update()
+                        except Exception:
+                            print(f"Erro ao encontrar o e-mail ou razão social para o CNPJ: {cnpj_limpo}")
+                            pass  # Se não achar, continua
+
+                        salvar_emails(resultados)
+                        sleep(12)
+
+        salvar_emails(resultados)
+    finally:
+        driver.quit()
+
+def consulta_cnpj_ja(label_contador, janela):
+    arquivos = glob.glob("cnpjs_*.xlsx")
+
+    if not arquivos:
+        messagebox.showwarning("Atenção", "O arquivo 'cnpjs_unicos.xlsx' não foi encontrado! Elimine os CNPJ duplicados!")
+        return
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    def limpar_cnpj(cnpj):
+        return ''.join(filter(str.isdigit, str(cnpj)))
+
+    total_emails = 0
+    resultados = []
+    emails_unicos = set()
+
+    try:
+        for arquivo in arquivos:
+            if os.path.exists(arquivo):
+                df = pd.read_excel(arquivo)
+
+                for cnpj in df.iloc[:, 0]:  # Segunda coluna
                     cnpj_limpo = limpar_cnpj(cnpj)
                     if cnpj_limpo:
                         link = f"https://cnpja.com/office/{cnpj_limpo}"
                         driver.get(link)
-                        sleep(3)  # Aumentando o tempo de espera
+                        sleep(3)
 
                         try:
                             email_element = WebDriverWait(driver, 10).until(
@@ -172,20 +165,23 @@ def consulta_cnpj_ja(label_contador, janela):
                             )
                             email = email_element.text.strip()
 
+                            # Capturando a razão social
+                            razao_social_element = driver.find_element(By.XPATH, '//div[@class="inline cursor-copy "]/span')
+                            razao_social = razao_social_element.text.strip()
+
                             if email not in emails_unicos:
                                 if email != 'contato@cnpja.com':
                                     emails_unicos.add(email)
-                                    resultados.append({"CNPJ": cnpj_limpo, "Email": email})
+                                    resultados.append({"CNPJ": cnpj_limpo, "Email": email, "Razao Social": razao_social})
                                     total_emails += 1
                                     label_contador.configure(text=f"E-mails raspados: {total_emails}")
                                     janela.update()
                         except Exception:
-                            print(f"Erro ao encontrar o e-mail para o CNPJ: {cnpj_limpo}")
-                            pass  # Se não achar o email, passa para o próximo CNPJ
-                        
+                            print(f"Erro ao encontrar o e-mail ou razão social para o CNPJ: {cnpj_limpo}")
+                            pass  # Se não achar, continua
+
                         salvar_emails(resultados)
                         sleep(12)
-                        
 
         salvar_emails(resultados)
     finally:
